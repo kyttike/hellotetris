1f6092bd91d0c231c4950e68065fdb1b0e092915
m2rtk <mrt.konsap@gmail.com>
2015-12-01 13:28:07 +0200

    yoyo

---
+++ tetris.py
@@ -0,0 +1,9 @@
+######################################
+#Tetris                              #
+#Märt Konsap ja Daniel Kütt          #
+#inf3                                #
+#2015                                #
+######################################
+
+import pygame, random, sys
+
