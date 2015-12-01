f62ecee72ad9bbee75ba103d1d5377b649cbe944
m2rtk <mrt.konsap@gmail.com>
2015-12-01 14:04:26 +0200

    WHAT

--- tetris.py
+++ tetris.py
@@ -9,11 +9,16 @@ import pygame, random, sys
 from pygame.locals import *
 
 
-AKNAKÕRGUS = 800
-AKNALAIUS  = 640
-KASTISUURUS= 20
-
-
+AKNAK6RGUS = 640
+AKNALAIUS  = 480
+KASTISUURUS= 25
+LAUALAIUS  = 10
+LAUAK6RGUS = 20
+# lauasuurus on kastisuuruse järgi, ehk laius on 10*25 = 250 pikslit
+TOP_BOT_ÄÄRIS = (AKNAK6RGUS - LAUAK6RGUS*KASTISUURUS)//2
+VASAK_ÄÄRIS   = KASTISUURUS
+PAREM_ÄÄRIS   = AKNALAIUS - (LAUALAIUS*KASTISUURUS + VASAK_ÄÄRIS)
+############### 480 - (10*25 + 25) = 205
 VALGE = (255,255,255)
 HALL  = (185,185,185)
 MUST  = (  0,  0,  0)
