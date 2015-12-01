21083ea217589dae369561524a69d50084897034
kyttike <danielkytt@hotmail.com>
2015-12-01 14:05:24 +0200

    summitxkcd

--- tetris.py
+++ tetris.py
@@ -14,7 +14,29 @@ AKNALAIUS  = 640
 KASTISUURUS= 20
 
 
-VALGE = (255,255,255)
-HALL  = (185,185,185)
-MUST  = (  0,  0,  0)
+VALGE       = (255,255,255)
+HALL        = (185,185,185)
+MUST        = (  0,  0,  0)
+TPUNANE     = (204,  0,  0)
+PUNANE      = (255,  0,  0)
+HPUNANE     = (255,102,102)
+TROHELINE   = (  0,204,  0)
+ROHELINE    = (  0,255,  0)
+HROHELINE   = (102,255,102)
+TORANZ      = (  0,  0,  0)
+ORANZ       = (  0,  0,  0)
+HORANZ      = (  0,  0,  0)
+TSININE     = (  0,  0,  0)
+SININE      = (  0,  0,  0)
+HSININE     = (  0,  0,  0)
+TKOLLANE    = (  0,  0,  0)
+KOLLANE     = (  0,  0,  0)
+HKOLLANE    = (  0,  0,  0)
+TAKVA       = (  0,  0,  0)
+AKVA        = (  0,  0,  0)
+HAKVA       = (  0,  0,  0)
+TLILLA      = (  0,  0,  0)
+LILLA       = (  0,  0,  0)
+HLILLA      = (  0,  0,  0)
+
 
